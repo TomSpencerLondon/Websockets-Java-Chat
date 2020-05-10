8cfa8e52c9012de061c630920baef8f0c93c72b8
Tom <thomas.spencer@codurance.com>
2020-05-10 23:22:35 +0100

    Add set up for spring boot app with static files for front end

---
+++ src/main/java/com/codurance/WebsocketsChat/model/MessageType.java
@@ -0,0 +1,7 @@
+package com.codurance.WebsocketsChat.model;
+
+public enum MessageType {
+    CHAT,
+    CONNECT,
+    DISCONNECT
+}
---
+++ src/test/java/com/codurance/WebsocketsChat/WebsocketsChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codurance.WebsocketsChat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketsChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
